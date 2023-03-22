e80a30281a008bb9f2c4b8002392639f6191bf3d
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-22 07:03:36 +0300

    new permission

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -33,3 +33,10 @@ class IsAdmin(BasePermission):
             request.user.is_admin
             or request.user.is_superuser
         )
+
+class IsAuthor(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
