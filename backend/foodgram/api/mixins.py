e67ad563ccd53e39ab821c59dfc4aab7811e884a
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-22 07:03:19 +0300

    new mixin

---
+++ backend/foodgram/api/mixins.py
@@ -0,0 +1,6 @@
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+
+class CreateDestroyViewSet(CreateModelMixin, DestroyModelMixin, GenericViewSet):
+    pass
