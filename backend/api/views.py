f9b8631d49c0668d01d492b748778a905ad10aca
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-26 08:46:45 +0300

    comment

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.http import FileResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.filters import SearchFilter
+# from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
