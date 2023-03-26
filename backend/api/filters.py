9d901a0a3256c6db469cf6ee53b82c3455c375b8
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-26 08:24:40 +0300

    fix

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -22,11 +22,11 @@ class RecipeFilter(FilterSet):
     def is_favorited_filter(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(favorite_recipe__user=user)
+            return queryset.filter(fav_recipe__user=user)
         return queryset
 
     def is_in_shopping_cart_filter(self, queryset, name, value):
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(shopping_recipe__user=user)
+            return queryset.filter(user_cart__user=user)
         return queryset
