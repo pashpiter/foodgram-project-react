8ddb86bd694989742047507c197bb086d766e3be
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-30 06:51:00 +0300

    fix

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (SubscriptionsViweSet,
+from .views import (GetSubscriptionsView, SubscribeViewSet,
                     UserCreateGetPatchViewSet)
 
 router = DefaultRouter()
@@ -10,12 +10,12 @@ router.register('users', UserCreateGetPatchViewSet, basename='users')
 urlpatterns = [
     path(
         'users/<author_id>/subscribe/',
-        SubscriptionsViweSet.as_view({'post': 'create', 'delete': 'destroy'}),
+        SubscribeViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
         name='subscribe'
     ),
     path(
         'users/subscriptions/',
-        SubscriptionsViweSet.as_view({'get': 'list'}),
+        GetSubscriptionsView.as_view({'get': 'list'}),
         name='getsubs'
     ),
     path('auth/', include('djoser.urls.authtoken')),
