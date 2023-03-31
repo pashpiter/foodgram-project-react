9708f7aa7847f121d5ff4a9001d0a8dac2369963
Pavel Drovnin <pavel98rus@gmail.com>
2023-03-31 04:13:01 +0300

    fix pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
     'PAGE_SIZE': 6,
     'SEARCH_PARAM': 'name',
 }
